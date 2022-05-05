ca4bd59cb1da866baa3af9aff8cb31f14c7804dd
Atanas Trifonov <77271452+tanagaHub@users.noreply.github.com>
2022-05-05 20:14:03 +0300

    Update weatherapi.js

--- weatherapi.js
+++ weatherapi.js
@@ -11,10 +11,7 @@ export async function getCurrentForecast(url) {
 
 
 
-            //let icon = obj.weather.icon
-          //  img.setAttribute('src', `https://www.weatherbit.io/static/img/icons/${icon}.png`)
-          //  p.innerHTML = `${obj.city_name} : ${obj.temp} &#x2103;` 
-
+           
 
 
 
